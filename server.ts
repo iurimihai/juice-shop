b4654e1e3ec11faeaa6194ef868db95f6a8c9363
Iuri Meresan <iurimihai.meresan@gmail.com>
2024-09-01 19:24:13 +0300

    Trigger pipeline

--- server.ts
+++ server.ts
@@ -707,3 +707,5 @@ export function close (exitCode: number | undefined) {
 // stop server on sigint or sigterm signals
 process.on('SIGINT', () => close(0))
 process.on('SIGTERM', () => close(0))
+
+
